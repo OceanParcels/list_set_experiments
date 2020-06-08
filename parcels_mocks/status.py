65ff490ff5e47d8336e0e3d68238d6d880d0229e
Christian Kehl <dj.kylix@googlemail.com>
2020-06-08 15:10:39 +0200

    re-defined and re-arranged the status with codes for particle-inherent merge and split operations

--- parcels_mocks/status.py
+++ parcels_mocks/status.py
@@ -1,12 +1,16 @@
 
 
 class StatusCode(object):
-    Success = 0
+    Success = 0                 # std. result; related to C 'EXIT_SUCCESS'
+    # ==== INTERNAL OPERATIONS ==== #
     Repeat = 1
     Delete = 2
-    Error = 3
-    ErrorInterpolation = 31
-    ErrorOutOfBounds = 4
-    ErrorThroughSurface = 41
-    ErrorTimeExtrapolation = 42
+    Merge = 3
+    Split = 4
+    # ==== ERROR CODES ==== #
+    Error = 5
+    ErrorInterpolation = 51
+    ErrorOutOfBounds = 6
+    ErrorThroughSurface = 61
+    ErrorTimeExtrapolation = 62
 
