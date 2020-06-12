9a327abd4e948f8bcb6cd13cf67592a43b992a61
Christian Kehl <dj.kylix@googlemail.com>
2020-06-12 18:54:34 +0200

    extended the mocks of Parcels fields and grids

--- parcels_mocks/field.py
+++ parcels_mocks/field.py
@@ -1,13 +1,52 @@
 from .grid import *
+from ctypes import c_float
+from ctypes import c_int
+from ctypes import POINTER
+from ctypes import pointer
+from ctypes import Structure
 import numpy as np
+
+from datetime import timedelta
+
+
+__all__ = ['Field', 'VectorField', 'SummedField', 'NestedField', 'FieldSet']
 
 
 class Field(object):
     data = None
 
-    def __init__(self, fieldset=None):
+    def __init__(self, fieldset=None, time=None, name="", grid=None):
         self.fieldset = fieldset
-        self.data = np.empty((2,2), dtype=np.float32)
+        self.name = name
+        self.data = np.empty((2, 2, 2), dtype=np.float32)
+        self.c_data = self.data.flatten(order='C')
+        self.c_data_ptr = None
+        self.time = np.arange(0, timedelta(days=365).total_seconds(), timedelta(days=10).total_seconds(), dtype=np.float64) if time is None else time
+        self.grid = Grid(self.time, 2, 2, 2, self.time.shape[0]) if grid is None else grid
+
+
+    @property
+    def ctypes_struct(self):
+        """Returns a ctypes struct object containing all relevant
+        pointers and sizes for this field."""
+        return self.get_ctypes_struct()
+
+    def get_ctypes_struct(self):
+        # Ctypes struct corresponding to the type definition in parcels.h
+        class CField(Structure):
+            _fields_ = [('xdim', c_int), ('ydim', c_int), ('zdim', c_int),
+                        ('tdim', c_int), ('data_chunks', POINTER(c_float)),
+                        ('grid', POINTER(CGrid))]
+
+        # Create and populate the c-struct object
+        if not self.c_data.flags.c_contiguous:
+            self.c_data = np.array(self.data.flatten(), dtype=self.data.dtype, order='C')
+        self.c_data_ptr = self.c_data.ctypes.data_as(POINTER(c_float))
+
+        cstruct = CField(self.grid.xdim, self.grid.ydim, self.grid.zdim,
+                         self.grid.tdim, self.c_data_ptr,  # (POINTER(POINTER(c_float)) * len(self.c_data))(*self.c_data),
+                         pointer(self.grid.ctypes_struct))
+        return cstruct
 
     def __getitem__(self, key):
         return self.eval(*key)
@@ -21,8 +60,14 @@ class Field(object):
         """
         return 0
 
+    def time_index(self, time):
+        return 0
+
+    def depth_index(self, depth, lat, lon):
+        return 0
+
     def computeTimeChunk_simple(self, time_index):
-        return self.computeTimeChunk()
+        return self.computeTimeChunk(self.data, time_index)
 
     def computeTimeChunk(self, data, time_index):
         """
@@ -56,10 +101,18 @@ class FieldSet(object):
     def __init__(self):
         self._data = []
         self.gridset = GridSet()
+        # self.time = np.float64(0)
 
     def __del__(self):
         del self._data[:]
         self._data = None
+
+    def get_fields(self):
+        return self._data
+
+    @property
+    def fields(self):
+        return self.get_fields()
 
     def append(self, obj):
         self._data.append(obj)
@@ -79,5 +132,18 @@ class FieldSet(object):
         :param time: Time around which the FieldSet chunks are to be loaded. Time is provided as a double, relatively to Fieldset.time_origin
         :param dt: time step of the integration scheme
         """
-        pass
+        signdt = np.sign(dt)
+        nextTime = np.infty if dt > 0 else -np.infty
+        for f in self.fields:
+            nextTime_loc = f.grid.computeTimeChunk(f, time, signdt)
+            nextTime = min(nextTime, nextTime_loc) if signdt >= 0 else max(nextTime, nextTime_loc)
+
+        if abs(nextTime) == np.infty or np.isnan(nextTime):  # Second happens when dt=0
+            return nextTime
+        else:
+            nSteps = int((nextTime - time) / dt)
+            if nSteps == 0:
+                return nextTime
+            else:
+                return time + nSteps * dt
 
--- parcels_mocks/grid.py
+++ parcels_mocks/grid.py
@@ -1,35 +1,138 @@
+import functools
+import sys
+from ctypes import c_double
+from ctypes import c_float
+from ctypes import c_int
+from ctypes import c_void_p
+from ctypes import cast
+from ctypes import POINTER
+from ctypes import pointer
+from ctypes import Structure
+from enum import IntEnum
 import numpy as np
+
+__all__ = ['CGrid', 'Grid', 'GridSet']
+
+
+class CGrid(Structure):
+    _fields_ = [('gtype', c_int),
+                ('grid', c_void_p)]
 
 
 class Grid(object):
     _cstruct = None
 
-    def __init__(self):
-        pass
+    def __init__(self, time, xdim, ydim, zdim, tdim):
+        self.gtype = 1
+        # self.depth
+        self.xdim = xdim
+        self.ydim = ydim
+        self.zdim = zdim
+        self.tdim = tdim
+        self.time = time
+        self._cstruct = None
+        self._initialized = False
+
+    @property
+    def ctypes_struct(self):
+        # This is unnecessary for the moment, but it could be useful when going will fully unstructured grids
+        self.cgrid = cast(pointer(self.child_ctypes_struct), c_void_p)
+        cstruct = CGrid(self.gtype, self.cgrid.value)
+        return cstruct
+
+    @property
+    def child_ctypes_struct(self):
+        """Returns a ctypes struct object containing all relevant
+        pointers and sizes for this grid."""
+
+        class CStructuredGrid(Structure):
+            # z4d is only to have same cstruct as RectilinearSGrid
+            _fields_ = [('xdim', c_int), ('ydim', c_int), ('zdim', c_int),
+                        ('tdim', c_int),
+                        # ('depth', POINTER(c_float)),
+                        ('time', POINTER(c_double))
+                        ]
+
+        # Create and populate the c-struct object
+        if not self._cstruct:  # Not to point to the same grid various times if grid in various fields
+            if not isinstance(self.periods, c_int):
+                self.periods = c_int()
+                self.periods.value = 0
+            self._cstruct = CStructuredGrid(self.xdim, self.ydim, self.zdim,
+                                           self.tdim,
+                                           # self.depth.ctypes.data_as(POINTER(c_float)),
+                                           self.time.ctypes.data_as(POINTER(c_double)))
+        return self._cstruct
 
     @property
     def cstruct(self):
         return self._cstruct
+
+    def reset_cstruct(self):
+        self._cstruct = None
+
+    def computeTimeChunk(self, field, time_value, signdt):
+        time_indices_now = np.array([False,])
+        time_indices_next = np.array([False, ])
+        time_now = 0
+        time_next = 0
+        if signdt >= 0:
+            time_indices_lower = self.time<time_value
+            time_indices_higher = self.time>time_value
+            # ==== NOW ==== #
+            time_indices_lower_ceil = np.concatenate((np.array([True,]), time_indices_lower[1:]))
+            time_indices_higher_int = np.concatenate((time_indices_higher[1:], np.array([True,])))
+            time_indices_now = np.logical_and(time_indices_lower_ceil, time_indices_higher_int)
+            # ==== NEXT ==== #
+            time_indices_higher_int = np.concatenate((np.array([True,]), time_indices_lower[:-1]))
+            time_indices_higher_ceil = np.concatenate((time_indices_higher[:-1], np.array([True,])))
+            time_indices_next = np.logical_and(time_indices_higher_int, time_indices_higher_ceil)
+        else:
+            time_indices_lower = self.time>time_value
+            time_indices_higher = self.time<time_value
+            # ==== NOW ==== #
+            time_indices_lower_int = np.concatenate((time_indices_lower[:-1], np.array([True,])))
+            time_indices_higher_ceil = np.concatenate((np.array([True,]), time_indices_higher[:-1]))
+            time_indices_now = np.logical_and(time_indices_lower_int, time_indices_higher_ceil)
+            # ==== NEXT ==== #
+            time_indices_lower_int = np.concatenate((np.array([True,]), time_indices_higher[1:]))
+            time_indices_lower_ceil = np.concatenate((time_indices_lower[1:], np.array([True,])))
+            time_indices_next = np.logical_and(time_indices_lower_int, time_indices_lower_ceil)
+        time_now = self.time[time_indices_now]
+        time_next = self.time[time_indices_next]
+        #if not self._initialized:
+        #    self._initialized = True
+        #    return time_now
+        return time_next
 
 
 
 
 class GridSet(object):
     time = []
+    ti = 0
 
     def __init__(self):
         self._data = []
         self.time = []
+        self.ti = 0
 
     def __del__(self):
         del self._data[:]
         self._data = None
 
+    @property
+    def grids(self):
+        return self._data
+
     def append(self, obj):
         self._data.append(obj)
 
-    def set_time(self, min_time, max_time, grid_dt):
+    def set_time_by_minmax(self, min_time, max_time, grid_dt):
         self.time = np.arange(min_time, max_time, grid_dt, dtype=np.float64)
+
+    def set_time_by_numpy(self, time):
+        self.time = time
 
     def dimrange(self, var_name):
         return self.time[0], self.time[-1]
@@ -43,6 +146,7 @@ class GridSet(object):
         :return:
         """
         pass
+
 
     @property
     def size(self):
