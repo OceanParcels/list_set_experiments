9d3fe65167fd1d1a770726ca97c738b1feeb2d99
Christian Kehl <c.kehl@uu.nl>
2021-02-04 17:06:52 +0100

    added a head-init file for be able to auto-parse the package

---
+++ parcels_nodes/__init__.py
@@ -0,0 +1,9 @@
+from parcels_nodes.package_globals import *  # noqa
+from parcels_nodes.parcels_mocks import *  # noqa
+from parcels_nodes.codegenerator import *  #noga
+from parcels_nodes.particle import *  # noqa
+from parcels_nodes.particleset_node import *  # noqa
+from parcels_nodes.kernelbase import *  # noqa
+from parcels_nodes.kernel import *  # noqa
+from parcels_nodes.Node import *  # noga
+from parcels_nodes.LinkedList import *  # noqa
